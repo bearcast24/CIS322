d127423ca58c80a4cc3de3cabce391f141d56d53
Kyile LeBlanc <kleblanc@uoregon.edu>
2017-01-23 15:43:29 -0800

    Adding creat tables to the repo

---
+++ sql/create_tables.sql
@@ -0,0 +1,5 @@
+#create_tables.sql
+#Kyile LeBlanc
+#CIS 322 #2
+
+#Use python to make tables from the project guidelines
---
+++ sql/import_test.sh
@@ -0,0 +1,6 @@
+print("INSERT INTO assets (description, asset_tag) VALUES ('item1', 'ATT001')")
+
+
+#python3 import_test.sh > insert.sql
+
+#psql lost -f insert.sql
