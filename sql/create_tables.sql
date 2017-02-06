83dd487cb15aabe6484deee1c1b5c5e84f3446a5
Kyile LeBlanc <kleblanc@uoregon.edu>
2017-02-05 18:08:14 -0800

    Changed int keys to serial keys

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,15 +7,15 @@
 --Asset Tables
 
 CREATE TABLE products (
-product_pk	integer primary key, 
-vendor text, 
+product_pk	serial primary key, 
+vendor varchar(128) not null, 
 description text, 
 alt_description text
 );
 
 
 CREATE TABLE assets (
-	asset_pk 	integer primary key, 
+	asset_pk 	serial primary key, 
 	product_fk 	integer REFERENCES products(product_pk), 
 	asset_tag 	text, 
 	description text, 
@@ -23,12 +23,12 @@ CREATE TABLE assets (
 	);
 
 CREATE TABLE vehicles (
-vehicle_pk integer primary key,
-asset_fk integer REFERENCES assets(asset_pk)
+vehicle_pk serial primary key,
+asset_fk integer REFERENCES assets(asset_pk) not null
 );
 
 CREATE TABLE facilities (
-facility_pk integer primary key,
+facility_pk serial primary key,
 fcode varchar(6),
 common_name text,
 location text
@@ -44,7 +44,7 @@ depart_dt timestamp
 
 
 CREATE TABLE convoys (
-convoy_pk integer primary key,
+convoy_pk serial primary key,
 request text,
 source_fk integer REFERENCES facilities(facility_pk),
 dest_fk integer REFERENCES facilities(facility_pk),
@@ -71,14 +71,14 @@ unload_dt timestamp
 --User Tables
 
 CREATE TABLE users (
-user_pk integer primary key,
+user_pk serial primary key,
 username text,
 active boolean
 );
 
 
 CREATE TABLE roles (
-role_pk integer primary key,
+role_pk serial primary key,
 title text
 );
 
@@ -107,13 +107,13 @@ comment text
 
 
 CREATE TABLE compartments (
-compartment_pk integer primary key,
+compartment_pk serial primary key,
 abbrv text,
 comment text
 );
  
 CREATE TABLE security_tags (
-tag_pk integer primary key,
+tag_pk serial primary key,
 level_fk integer    not null        REFERENCES levels(level_pk),
 compartment_fk integer    not null  REFERENCES compartments(compartment_pk),
 user_fk integer     REFERENCES users(user_pk),
