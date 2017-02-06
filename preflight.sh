5a06b61677cf8641ea543ac4b72cb1095f7086bf
Kyile LeBlanc <kleblanc@uoregon.edu>
2017-02-06 00:26:54 -0800

    Fixed some bugs

--- preflight.sh
+++ preflight.sh
@@ -4,36 +4,22 @@
 #W3 
 #CIS 322
 
+#Calling program:
+#bash ./preflight.sh lost
+# Goals:
+#    1. creates the database
+#    2. imports the legacy data <-Sort of
+#    3. Copies the source files to $HOME/wsgi to serve webapp
 
-# Write a script named preflight.sh and commit the script at the top level of your repository (the same directory your install_daemons.sh script should be located). 
-
-# The preflight.sh script handles preconfiguration of your application. 
-# The script will be called after the Postgres server is running and a new database has been created. 
-# preflight.sh will be responsible for creating the tables needed by your LOST implementation 
-# in that database and loading enough data for your LOST implementation's functionality to be tested. 
-
-# If you are using mod_wsgi, preflight.sh should also copy the needed files into the $HOME/wsgi directory.
-
-
-bash ./preflight.sh lost
-
-
-
-
-
-#FROM CLASS REPO:
 if [ "$#" -ne 1 ]; then
     echo "Usage: ./preflight.sh <dbname>"
     exit;
 fi
 
-# Database prep
+# Make Database
 cd sql
-psql $1 -f create_tables.sql
-curl -O https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
-tar -xzf osnap_legacy.tar.gz
-bash ./import_data.sh $1 5432
-rm -rf osnap_legacy osnap_legacy.tar.gz
+psql -f create_tables.sql $1 
+psql -f import_data.sql $1 -p5432
 cd ..
 
 # Install the wsgi files
