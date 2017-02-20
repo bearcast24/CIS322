a3cc0ef6ced39511f9f6efe22c9b3bd0d9fa4858
Kyile LeBlanc <kleblanc@uoregon.edu>
2017-02-20 02:07:57 -0800

    Updates the txt requirments

--- preflight.sh
+++ preflight.sh
@@ -4,8 +4,6 @@
 #W3 
 #CIS 322
 
-
-
 # Goals:
 #    1. creates the database
 
@@ -17,5 +15,17 @@ fi
 # Database prep
 # All that this preflight script needs to do is set up the db
 # Andy's script already drops and creates db
+
+	# psql $1 -f create_tables.sql
+	# curl -O https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
+	# tar -xzf osnap_legacy.tar.gz
+	# bash ./import_data.sh $1 5432
+	# rm -rf osnap_legacy osnap_legacy.tar.gz
+
 cd sql
 psql $1 -f create_tables.sql
+cd ..
+
+
+# Install the wsgi files
+cp -R src/* $HOME/wsgi
