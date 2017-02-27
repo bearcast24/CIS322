204500e12b0abcd4819aaf6eb4743245e47f107f
Kyile LeBlanc <kleblanc@uoregon.edu>
2017-02-26 22:30:02 -0800

    Small to make the app work for

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import psycopg2
+import datetime
+import sys
 import json
 #This should pull all configuations from the json file:
 from config import dbname, dbhost, dbport
@@ -8,26 +10,24 @@ app = Flask(__name__)
 
 app.secret_key = 'secret_password'
 
-#Connect to postgres:
-conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
-cur  = conn.cursor()
-
 #Enter and Exit LOST
 @app.route('/', methods= ['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'GET': #Need to store login state??
-		return redirect(url_for('login.html'))
+    if request.method == 'GET': #Need to store login state??
+        return render_template('login.html')
 
-	elif request.method == 'POST':
-		uname = request.form['username']		
-		pwd = request.form['password']
-		
+    elif request.method == 'POST':
+        uname = request.form['username']        
+        pwd = request.form['password']
+        #Connect to postgres:
+        conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
+        cur  = conn.cursor()
         #queries:
         cur.execute("SELECT username,password FROM user_accounts WHERE username = '{}' and password = '{}'".format(uname, pwd))
         #If user is found:
         if cur.fetchone() is not None:
-        	session['username'] = uname
+            session['username'] = uname
             return render_template('dashboard.html')
         #If no user is found:
         return render_template('no_user.html')
@@ -44,15 +44,15 @@ def dashboard():
 
 @app.route('/create_user', methods=['GET', 'POST'])
 def create_user():
-	if request.method == 'GET':
-		return render_template('create_user.html')
-	
-	elif request.method == 'POST':
-		uname = request.form['username']		
-		pwd = request.form['password']
-		session['username'] = uname
-		#Connect to postgres:
-		conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
+    if request.method == 'GET':
+        return render_template('create_user.html')
+    
+    elif request.method == 'POST':
+        uname = request.form['username']        
+        pwd = request.form['password']
+        session['username'] = uname
+        #Connect to postgres:
+        conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
         cur  = conn.cursor()
         #queries:
         cur.execute("SELECT username, password FROM user_accounts where username = '{}' AND password = '{}'".format(uname, pwd))
@@ -61,9 +61,9 @@ def create_user():
         if cur.fetchone() is not None:
             return render_template('user_exists.html')
         else: 
-        	cur.execute("INSERT INTO user_accounts(username,password) VALUES ('{}', '{}');"..format(uname, pwd))
-        	conn.commit()
-        	return render_template('user_added.html')
+            cur.execute("INSERT INTO user_accounts(username,password) VALUES ('{}', '{}');"..format(uname, pwd))
+            conn.commit()
+            return render_template('user_added.html')
 
 
 if __name__=='__main__':
