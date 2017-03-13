0336bad0a1e2bfc245d4797cca1df881b35faa36
Kyile LeBlanc <kleblanc@uoregon.edu>
2017-03-13 02:58:03 -0700

    Typo in formating

--- import/import_data.py
+++ import/import_data.py
@@ -38,7 +38,7 @@ def import_facilities(facilities_file):
         reader = csv.DictReader(csvfile)
 
         for item in reader:
-            cur.execute("INSERT INTO facilities (fcode, common_name) VALUES( {},{} );".format(item['fcode'], item['common_name']))
+            cur.execute("INSERT INTO facilities (fcode, common_name) VALUES({},{});".format(item['fcode'], item['common_name']))
         conn.commit()
 
 
